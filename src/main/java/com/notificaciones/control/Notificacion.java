bf1434e85f07266ea83eb8059a88eef2aa74e2ea
Raúl Barragán <99059912+rb4rr4g4n00@users.noreply.github.com>
2022-05-15 19:23:50 -0500

    Notificaciones correo formularioAdopcion

---
+++ src/main/java/com/notificaciones/control/Notificacion.java
@@ -0,0 +1,24 @@
+package com.notificaciones.control;
+
+public class Notificacion {
+    
+    public Notificacion(String nombre, String correo){
+        PlantillaNotificacionesControl plantillaNotificacionesControl = new PlantillaNotificacionesControl(
+            PlantillaNotificacionesControl.PLANTILLA_NOTIFICACIONES_ADOPCION,
+            nombre
+        );
+
+        String [] correosCopia = {};
+        
+        NotificacionesControl notificacionesControl = new NotificacionesControl(
+            correo,
+            correosCopia,
+            "Notificaci\u00F3n " + " NoPetsInTheOcean",
+            plantillaNotificacionesControl.getCuerpoPlantilla()
+        );
+        notificacionesControl.enviarCorreo();
+    }
+    
+    
+    
+}
